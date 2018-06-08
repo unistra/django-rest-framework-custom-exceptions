8ae2cbac5b61f067945e6caf53f64beca2b343c1
tschlecht <thierry.schlecht@gmail.com>
2018-06-08 15:41:21 +0200

    Add suport for recent versions of DRF and django

--- tests/urls.py
+++ tests/urls.py
@@ -1,9 +1,8 @@
-from django.conf.urls import patterns, url
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.conf.urls import url
 from . import views
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^items', views.ItemList.as_view()),
     url(r'^not-items', views.NotItemList.as_view()),
     url(r'^forbidden-items', views.ForbiddenItemList.as_view()),
-)
+]
